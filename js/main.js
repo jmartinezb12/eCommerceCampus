64930b8a816978ad959c76ac98ee80132684caef
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-06-16 17:34:52 -0500

    feat(Variables): :sparkles: Agregar variables para el funcionamiento de la función addEventListener
    Se han agregado variables que ayudan a la función addEventListener a recibir todas las categorías. Estas variables permiten almacenar y manejar los datos necesarios para la correcta ejecución de la función.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+import { menuListCategoryIndex } from "./components/menu.js";
+import { galleryIndex } from "./components/gallery.js";
+import { getAllProductName, getAllCategory } from "./module/app.js";
+
+let input__search = document.querySelector("#input__search");
+let main__article = document.querySelector(".main__article");
+let nav__ul = document.querySelector(".nav__ul");
+
 addEventListener("DOMContentLoaded", async e=>{
     if(!localStorage.getItem("getAllCategory")) localStorage.setItem("getAllCategory", JSON.stringify(await getAllCategory()));
     nav__ul.innerHTML = await menuListCategoryIndex(JSON.parse(localStorage.getItem("getAllCategory")));  
