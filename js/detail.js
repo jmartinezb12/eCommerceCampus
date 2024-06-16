a26a3fd892d3f0f4690759bb483c404b7189b396
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-06-16 18:11:01 -0500

    feat(EventListener): :sparkles: Agregar función de evento de escucha para cambiar contenido del documento
    Se ha agregado la función de evento de escucha utilizando addEventListener con el tipo 'DOMContentLoaded' en forma asíncrona. Esta función permite cambiar el contenido del documento, específicamente en la sección de galería y título, mediante la carga de un JSON.

--- js/detail.js
+++ js/detail.js
@@ -1,4 +1,31 @@
-
+import { galleryCategory } from "./components/gallery.js";
+import { titleProductDetail } from "./components/section.js";
+import { getProductId } from "./module/detail.js";
 
 let main__section_gallery = document.querySelector("#main__section__gallery");
 let main__section__title = document.querySelector("#main__section__title");
+
+addEventListener("DOMContentLoaded", async(e)=>{
+    let params = new URLSearchParams(location.search);
+    let id = params.get('id');
+    if(!localStorage.getItem(id)) localStorage.setItem(id, JSON.stringify(await getProductId({id})));
+    
+    let info = JSON.parse(localStorage.getItem(id));
+    main__section_gallery.innerHTML = await galleryCategory(info);
+    main__section__title.innerHTML = await titleProductDetail(info);
+    
+
+    // let {data} = res;
+    // let {
+    //     category_path,
+    //     about_product,
+    //     product_details,
+    //     product_information,
+    //     product_photos,
+    //     product_variations,
+    //     rating_distribution,
+    //     review_aspects,
+    //     ...dataUpdate
+    // } = data;
+    // console.log(dataUpdate);
+})
