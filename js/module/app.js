1c8d66578149c5f9d0e38d40f1b5d0c1450810ea
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-06-17 22:14:24 -0500

    feat(Products): :sparkles: Implementar función getAllProductName para obtener productos con detalles
    Se ha implementado la función getAllProductName que utiliza el método GET para obtener todos los productos con sus respectivos detalles.

---
+++ js/module/app.js
@@ -0,0 +1,14 @@
+import { headers } from "../components/env.js";
+
+export const getAllProductName = async({search:text, id:idCategory})=>{
+    console.log("Esperando .......");
+    console.log(text, idCategory);
+    const url = `https://real-time-amazon-data.p.rapidapi.com/search?query=${text}&page=1&country=US&sort_by=RELEVANCE&category_id=${idCategory}&product_condition=ALL`;
+    const options = {
+        method: 'GET',
+        headers
+    };
+    let res = await fetch(url, options);
+    let data = res.json();
+    return data;
+}
