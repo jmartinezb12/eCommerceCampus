33b3b87a07d54fd8b407384e426d9f93ddc066e2
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-06-17 22:19:04 -0500

    feat(Categories): :sparkles: Implementar función getAllCategory para obtener categorías con detalles
    Se ha implementado la función getAllCategory que utiliza el método GET para obtener todas las categorías con sus respectivos detalles.

--- js/module/app.js
+++ js/module/app.js
@@ -12,3 +12,15 @@ export const getAllProductName = async({search:text, id:idCategory})=>{
     let data = res.json();
     return data;
 }
+
+export const getAllCategory = async()=>{
+    console.log("Esperando .......");
+    const url = `https://real-time-amazon-data.p.rapidapi.com/product-category-list?country=US`;
+    const options = {
+        method: 'GET',
+        headers
+    };
+    let res = await fetch(url, options);
+    let data = res.json();
+    return data;
+}
