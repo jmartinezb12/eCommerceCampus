59d0bc3bc35ddc284ef5aca8ad90400faa3c6a99
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-06-19 08:27:13 -0500

    refactor: :recycle: Reestructurar código en función quantity
    Se ha reestructurado el código en la función quantity en el archivo js/module/detail.js para mejorar la organización y legibilidad del código.

--- js/module/detail.js
+++ js/module/detail.js
@@ -12,11 +12,28 @@ export const getProductId = async({id:idCategory})=>{
     return data;
 }
 
-const quantity = async (e) => {
+const quantity = async (e)=>{
+    let span_quantity = document.querySelector("#span_quantity");
+    let price_discount = document.querySelector("#price_discount");
+    let price_original = document.querySelector("#price_original");
     let params = new URLSearchParams(location.search);
     let id = params.get('id');
     let res = JSON.parse(localStorage.getItem(id)).data;
 
-    let product_original_price = 0;
-    if(res.pro)
+    let product_original_price = undefined;
+    if(res.product_original_price) product_original_price = Number(res.product_original_price.replace("$", ""));
+    let product_price= Number(res.product_price.replace("$", ""));
+
+    
+    if(e.target.id == "btn_plus")span_quantity.innerHTML = Number(span_quantity.innerHTML) + 1
+    if(e.target.id == "btn_minus" && span_quantity.innerHTML > "1") span_quantity.innerHTML = Number(span_quantity.innerHTML) - 1;
+
+    price_discount.innerHTML = `$${(product_price * Number(span_quantity.innerHTML)).toFixed(2)}`;
+    if(product_original_price) price_original.innerHTML = `$${(product_original_price * Number(span_quantity.innerHTML)).toFixed(2)}`;
+    // Swal.fire({
+    //     position: "top-end",
+    //     title: `<small>Product ${id} with a quantity of ${span_quantity.innerHTML} was added to the cart</small>`,
+    //     showConfirmButton: false,
+    //     timer: 2000
+    // });
 }
