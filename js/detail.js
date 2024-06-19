85e680eee4d90cfec8350b189c349b94974cadcb
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-06-19 07:52:38 -0500

    feat: :sparkles: Recibir y actualizar cantidades en el carrito de compra
    Se ha implementado la funcionalidad para recibir y actualizar las cantidades de productos en el carrito de compra, dependiendo de los eventos de presionar "más" o "menos".

--- js/detail.js
+++ js/detail.js
@@ -36,3 +36,29 @@ addEventListener("DOMContentLoaded", async(e)=>{
     btn_minus.addEventListener("click",quantity)
     btn_plus.addEventListener("click",quantity)
 })
+
+const quantity = async (e)=>{
+    let span_quantity = document.querySelector("#span_quantity");
+    let price_discount = document.querySelector("#price_discount");
+    let price_original = document.querySelector("#price_original");
+    let params = new URLSearchParams(location.search);
+    let id = params.get('id');
+    let res = JSON.parse(localStorage.getItem(id)).data;
+
+    let product_original_price = undefined;
+    if(res.product_original_price) product_original_price = Number(res.product_original_price.replace("$", ""));
+    let product_price= Number(res.product_price.replace("$", ""));
+
+    
+    if(e.target.id == "btn_plus")span_quantity.innerHTML = Number(span_quantity.innerHTML) + 1
+    if(e.target.id == "btn_minus" && span_quantity.innerHTML > "1") span_quantity.innerHTML = Number(span_quantity.innerHTML) - 1;
+
+    price_discount.innerHTML = `$${(product_price * Number(span_quantity.innerHTML)).toFixed(2)}`;
+    if(product_original_price) price_original.innerHTML = `$${(product_original_price * Number(span_quantity.innerHTML)).toFixed(2)}`;
+    // Swal.fire({
+    //     position: "top-end",
+    //     title: `<small>Product ${id} with a quantity of ${span_quantity.innerHTML} was added to the cart</small>`,
+    //     showConfirmButton: false,
+    //     timer: 2000
+    // });
+}
